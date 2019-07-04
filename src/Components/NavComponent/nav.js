fa151fc24cad6e13986f1d7c145a686325b6bfbc
Heather Barr <hnbarr@yahoo.com>
2019-07-04 16:52:41 -0500

    working on final I may start over since I have some new ideas

--- src/Components/NavComponent/nav.js
+++ src/Components/NavComponent/nav.js
@@ -24,45 +24,38 @@ export default class NavComponent extends Component {
         return (
             <div id='navbar'>
             <Navbar light expand="md">
-                <NavbarBrand href="/">  </NavbarBrand> 
-                {/* {<i className="fas fa-chevron-up"></i> &nbsp; stand up} */}
+                <NavbarBrand href="/"> <i className="fas fa-chevron-up"></i> &nbsp; stand up </NavbarBrand> 
+
                 
                 <NavbarToggler onClick={this.toggle} />
                 <Collapse isOpen={this.state.isOpen} navbar>
                 
                     <Nav className="ml-auto" navbar>
-                
-                        {/* <NavItem>
+                    
+                        <NavItem>
                             <NavLink href="https://github.com/AustinCodingAcademy"> <i className="fab fa-github"></i> GitHub</NavLink>
                         </NavItem>
-                        took out campus manager here
-                        <NavItem>
-                            <NavLink href="/link-to-resume"> <i className="far fa-file"></i> Resume </NavLink>
-                        </NavItem> */}
+
                         <UncontrolledDropdown nav inNavbar>
                             <DropdownToggle nav caret>
-                               Destinations
+                               Resources
                             </DropdownToggle>
                             <DropdownMenu right>
-                            {/* took out articles and videos here */}
                             <DropdownItem>
-                            <NavLink href="/articles"><i className="fas fa-beer"></i> Bars </NavLink>
+                            <NavLink href="https://youtube.com"><i className="fab fa-youtube"></i> Youtube </NavLink>
                             </DropdownItem>
+
                             <DropdownItem>
-                                <NavLink href="/videos"><i className="fas fa-utensils"></i> Restaurants </NavLink>
+                                <NavLink href="https://stackoverflow.com"><i className="fas fa-question-circle"></i> StackOverflow </NavLink>
                             </DropdownItem>
+                            
                             <DropdownItem>
-                                <NavLink href="/whiteboarding"><i className="fas fa-hiking"></i> Trails </NavLink>
+                                <NavLink href="/notes"><i className="fas fa-book-open"></i> Notes </NavLink>
                             </DropdownItem>
-                            <DropdownItem>
-                                <NavLink href="https://austincodingacademy.com/forum/"> <i className="fas fa-tree"></i> Parks</NavLink>
-                            </DropdownItem>
-                            {/* <DropdownItem>
-                                <NavLink href="https://campus.austincodingacademy.com/"> <i className="fas fa-book-open"></i> Campus Manager</NavLink>
-                            </DropdownItem> */}
+                            
                             <DropdownItem divider />
                             <DropdownItem>
-                                <NavLink href="/account"> <i className="far fa-user-circle"></i> Help</NavLink>
+                                <NavLink href="/account"> <i className="far fa-user-circle"></i> Logout</NavLink>
                             </DropdownItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
@@ -74,14 +67,6 @@ export default class NavComponent extends Component {
     }
 }
 
-
-
-{/* <DropdownItem>
-    <NavLink href="/articles"><i className="fas fa-book-open"></i> Articles</NavLink>
-</DropdownItem>
-<DropdownItem>
-    <NavLink href="/videos"><i className="fab fa-youtube"></i> Videos</NavLink>
-</DropdownItem> */}
 
 {/* <NavItem>
     <NavLink href="https://campus.austincodingacademy.com/">Campus Manager</NavLink>
--- src/Components/WelcomeComponent/index.js
+++ src/Components/WelcomeComponent/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Jumbotron, Container, Button } from 'reactstrap';
 import './Welcome.css'
-// import Articles from '../Article';
-// import Media from '../Media';
+import Articles from '../Article';
+import Media from '../Media';
 
 const Welcome = (props) => {
   const handleClick = (e) => {
@@ -10,31 +10,22 @@ const Welcome = (props) => {
   }
 
   return (
-    <div>
+    <React.Fragment>
       <Jumbotron className='jumbo' fluid>
         <Container fluid>
-          <h1 className="display-3">Pup n Go</h1>
-          <h4>Here we can find austin dog-friendly bars, restaurants, parks and trails! </h4>
+          <h1 className="display-3">daily tasks</h1>
           <form id='focus_section'>
-            {/* <p className='text'></p> */}
             <input id='focus_field' placeholder='anything special you looking for?' type='text'></input>
           </form>
           <p className='text'> or select one here!</p>
-          {/* <Button onClick={handleClick} outline color="secondary">React </Button>{' '}
-          <Button onClick={handleClick} outline color="secondary">Redux </Button>{' '}
-          <Button onClick={handleClick} outline color="secondary">Mongo DB </Button>{' '}
-          <Button onClick={handleClick} outline color="secondary">Express </Button>{' '}
-          <Button onClick={handleClick} outline color="secondary">Authentication </Button>{' '}
-          <Button onClick={handleClick} outline color="secondary">Whiteboarding</Button>{' '} */}
-          {/* <i className="text_slant">To delete a focus, click it! (Limit: 3)</i> */}
        </Container>
       </Jumbotron>
 
-      {/* <section id='media_bottom'>
+      <section id='media_bottom'>
         <Articles />
         <Media />
-      </section> */}
-    </div>
+      </section>
+    </React.Fragment>
   );
 };
 
